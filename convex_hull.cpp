b94d8af9bd669f403f951ea9ca7971fff2afed64
ItakEjgo <137280994@qq.com>
2019-12-09 16:42:19 +0800

    upload chapter1 code

---
+++ convex_hull.cpp
@@ -0,0 +1,77 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+const double eps = 1e-9;
+int sign(const double x){
+    if (fabs(x) < eps) return 0;
+    return x < 0 ? -1 : 1;
+}
+
+struct Point{
+    double x, y;
+    Point(){}
+    Point(double x, double y):x(x), y(y){}
+    Point operator + (const Point &p) const { return Point(x + p.x, y + p.y); }
+    Point operator - (const Point &p) const { return Point(x - p.x, y - p.y); }
+    double operator ^ (const Point &p) const { return x * p.y - y * p.x; }
+    double operator * (const Point &p) const { return x * p.x + y * p.y; }
+    bool operator < (const Point &p) const{ return x < p.x || x == p.x && y < p.y; }
+};
+
+typedef vector<Point> VP;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+
+VP P;
+
+//check whether r lies left or on the line pq
+bool lies_left(Point p, Point q, Point r) {
+    return sign((q - p) ^ (r - p)) > 0 ? 1 : 0;
+}
+
+bool cmp(const pii &a, const pii &b) {
+    return sign((P[a.second] - P[a.first]) ^ (P[b.second] - P[a.first])) <= 0;
+}
+
+VP convex_hull(VP P){
+    sort(P.begin(), P.end());
+    VP ret = {};
+    int sz = 0;
+    for (int i = 0; i < P.size(); i++) {
+        cout << "xx: " << P[i].x << " " << P[i].y << endl;
+        while (sz > 1 && !lies_left(ret[sz - 2], P[i], ret[sz - 1])) {
+            ret.pop_back();
+            sz--;
+        }
+        cout << "push in: " << P[i].x << " " << P[i].y << endl;
+        ret.push_back(P[i]);
+        sz++;
+    }
+    int k = sz;
+    for (int i = P.size() - 2; i >= 0; i--){
+        cout << "yy: " << P[i].x << " " << P[i].y << endl;
+        while (sz > k && !lies_left(ret[sz - 2], P[i], ret[sz - 1])) {
+            ret.pop_back();
+            sz--;
+        }
+        ret.push_back(P[i]);
+        sz++;
+    }
+    ret.pop_back();
+    sz--;
+    return ret;
+}
+
+int main(){
+    int n; cin >> n;
+    for (int i = 0; i < n; ++i){
+        double x, y; cin >> x >> y;
+        P.push_back(Point(x, y));
+    }
+    VP res = convex_hull(P);
+    for (int i = 0; i < res.size(); i++){
+        cout << res[i].x << " " << res[i].y << endl;
+    }
+    return 0;
+}
---
+++ slow_convex_hull.cpp
@@ -0,0 +1,77 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+const double eps = 1e-9;
+int sign(const double x){
+    if (fabs(x) < eps) return 0;
+    return x < 0 ? -1 : 1;
+}
+
+struct Point{
+    double x, y;
+    Point(){}
+    Point(double x, double y):x(x), y(y){}
+    Point operator + (const Point &p) const { return Point(x + p.x, y + p.y); }
+    Point operator - (const Point &p) const { return Point(x - p.x, y - p.y); }
+    double operator ^ (const Point &p) const { return x * p.y - y * p.x; }
+    double operator * (const Point &p) const { return x * p.x + y * p.y; }
+    bool operator == (const Point &p) const { return !sign(x - p.x) && !sign(y - p.y);}
+};
+
+typedef vector<Point> VP;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+
+Point origin;
+VP P;
+
+//check whether r lies left of line pq
+bool lies_left(Point p, Point q, Point r) {
+    return sign((q - p) ^ (r - p)) > 0 ? 1 : 0;
+}
+
+bool cmp(const Point &a, const Point &b) {
+    //return sign(angle(a) - angle(b)) < 0;
+    return sign((a - origin) ^ (b - origin)) < 0;
+}
+
+VP slow_convex_hull(VP P){
+    VP ret = {};
+    for (int i = 0; i < P.size(); i++){
+        for (int j = 0; j < P.size(); j++){
+            if (i == j) continue;
+            bool valid = 1;
+            for (int r = 0; r < P.size(); r++){
+                if (r == i || r == j) continue;
+                if (lies_left(P[i], P[j], P[r])) {
+                    valid = 0;
+                    break;
+                }
+            }
+            if (valid) ret.push_back(P[i]);
+        }
+    }
+    origin = ret[0];
+    cout << origin.x << " " << origin.y << endl;
+    sort(ret.begin(), ret.end(), cmp);
+    VP res = {}; 
+    res.push_back(ret[0]);
+    for (int i = 1; i < ret.size(); i++){
+       if (!(ret[i] == ret[i - 1])) res.push_back(ret[i]); 
+    }
+    return res;
+}
+
+int main(){
+    int n; cin >> n;
+    for (int i = 0; i < n; ++i){
+        double x, y; cin >> x >> y;
+        P.push_back(Point(x, y));
+    }
+    VP res = slow_convex_hull(P);
+    for (int i = 0; i < res.size(); i++){
+        cout << res[i].x << " " << res[i].y << endl;
+    }
+    return 0;
+}
